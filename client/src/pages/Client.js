8f6d967e410b66d4c9b0783a7ee073a6363c5c7d
AngeloPepito2127 <74907670+AngeloPepito2127@users.noreply.github.com>
2021-11-24 23:17:59 +0800

    dont take this

--- client/src/pages/Client.js
+++ client/src/pages/Client.js
@@ -162,6 +162,7 @@ const Client = () => {
       // socketRef.current = io.connect("/");
       // socketRef.current = io('http://localhost:5000');
       socketRef.current = io('https://eie4428-webcam-app.herokuapp.com/');
+      socketRef.current = io('http://localhost:5000');
       navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
           userVideo.current.srcObject = stream;
           socketRef.current.emit("server join room", roomID);
